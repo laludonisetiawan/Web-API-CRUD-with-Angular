aee4565efe77e6758fbaf214d8286693146bcfb6
laludonisetiawan <laludonisetiawan@gmail.com>
2025-01-25 22:29:05 +0700

    Tambah service untuk ambil data pengguna dari API

--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentDetailService {
+  url: string = environment.apiBaseUrl + '/PaymentDetail';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  refreshList() {
+    this.http.get(this.url).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
