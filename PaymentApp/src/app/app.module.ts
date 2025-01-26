e6bdace0d9be7258549cfe007c3080e052017bbc
laludonisetiawan <laludonisetiawan@gmail.com>
2025-01-26 23:50:28 +0700

    update return API CRUD, and setup configuration toastr notification popup in angularjs

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +18,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 import { NgForm } from '@angular/forms';
 import { PaymentDetail } from 'src/app/shared/payment-detail.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-payment-detail-form',
@@ -9,13 +10,14 @@ import { PaymentDetail } from 'src/app/shared/payment-detail.model';
   styleUrls: ['./payment-detail-form.component.css'],
 })
 export class PaymentDetailFormComponent {
-  constructor(public service: PaymentDetailService) {}
+  constructor(public service: PaymentDetailService, private toastr: ToastrService) {}
 
   onSubmit(form: NgForm) {
     this.service.postPaymentDetail().subscribe({
       next: (res) => {
         this.service.list = res as PaymentDetail[];
         this.service.resetForm(form);
+        this.toastr.success('Submitted successfully', 'Payment Detail Register');
       },
       error: (err) => {
         console.log(err);
